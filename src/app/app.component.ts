05d039769491e7b7043e5e2642ade337a5cacf64
xwang985 <39933823+xwang985@users.noreply.github.com>
2020-02-16 18:12:46 -0500

    product management module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,15 @@ import { UserService } from './user.service'
 export class AppComponent {
   constructor(private auth: AuthService, router: Router, private userService: UserService) {
   	auth.user$.subscribe(user => {
-  		if (user) {
-        userService.save(user);
+      if (!user) return
+      
+      userService.save(user);
 
-  			let returnUrl = localStorage.getItem('returnUrl');
-  			router.navigateByUrl(returnUrl);
-  		}
+			let returnUrl = localStorage.getItem('returnUrl');
+      if (!returnUrl) return
+        
+      localStorage.removeItem('returnUrl');
+      router.navigateByUrl(returnUrl);
   	});
   }
 
